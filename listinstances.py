01da58156f901e42730cc35e0c22205a109e8f97
Karthic Ravindran <karthicoracle@gmail.com>
2022-06-08 15:35:05 +0530

    exception block added

--- listinstances.py
+++ listinstances.py
@@ -12,14 +12,16 @@ compute_client = oci.core.ComputeClient({},signer=signer)
 network_client = oci.core.VirtualNetworkClient({},signer=signer)
 identity_client = oci.identity.IdentityClient({},signer=signer)
  
-resp = search_client.search_resources(
+try:
+  resp = search_client.search_resources(
   oci.resource_search.models.StructuredSearchDetails(
   type="Structured",
-  query="query instance resources"
+  query="query instance resources where lifeCycleState != 'TERMINATED'"
   )
 )
+except Exception as ex:
+  print(ex)
 
- 
 for instance in resp.data.items:
   resp = compute_client.list_vnic_attachments(
   compartment_id=instance.compartment_id,
@@ -27,19 +29,22 @@ for instance in resp.data.items:
   )
  
   for vnic_attachment in resp.data:
-    vnic = network_client.get_vnic(vnic_attachment.vnic_id).data
-    compute = compute_client.get_instance(instance_id=instance.identifier).data
-    compartment_details = identity_client.get_compartment(compartment_id=instance.compartment_id).data
-    print(",".join([
-    compute.display_name,
-    vnic.private_ip,
-    str(vnic.public_ip),
-    compute.lifecycle_state,
-    str(instance.time_created),
-    compartment_details.name,
-    compute.shape,
-    str(compute.shape_config.ocpus),
-    str(compute.shape_config.memory_in_gbs),
-    compute.availability_domain,
-    compute.fault_domain
-    ]))
+    try:
+      vnic = network_client.get_vnic(vnic_attachment.vnic_id).data
+      compute = compute_client.get_instance(instance_id=instance.identifier).data
+      compartment_details = identity_client.get_compartment(compartment_id=instance.compartment_id).data
+      print(",".join([
+      str(compute.display_name),
+      str(vnic.private_ip),
+      str(vnic.public_ip),
+      str(compute.lifecycle_state),
+      str(instance.time_created),
+      str(compartment_details.name),
+      str(compute.shape),
+      str(compute.shape_config.ocpus),
+      str(compute.shape_config.memory_in_gbs),
+      str(compute.availability_domain),
+      str(compute.fault_domain)
+      ]))
+    except Exception as ex1:
+      print(ex1)
