7af47935a35fa7aba1d5a012a620abc92541ac87
KarthicRavindran <karthic.ravindran@oracle.com>
2023-07-14 12:21:47 +0530

    Update ORMdriftdetection.sh

--- ORMdriftdetection.sh
+++ ORMdriftdetection.sh
@@ -6,9 +6,6 @@ final_stack_list=`echo $stack_list |tr -d "'"`
 for stack in $final_stack_list
 do
     oci resource-manager stack detect-drift --stack-id $stack --wait-for-state SUCCEEDED --wait-for-state FAILED --wait-interval-seconds 10
-done
-for stack in $final_stack_list
-do
     sync_status=`oci resource-manager stack get --stack-id $stack|grep "stack-drift-status"| awk '{print $2}'|tr -d ','`
     if [ $sync_status != '"IN_SYNC"' ]
     then
